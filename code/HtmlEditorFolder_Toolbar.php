83c52d3d9cf56bd29630a48a1139f781d1a81d70
Brian Cairns <brianc@evanshunt.com>
2017-05-10 22:19:23 -0600

    Set default folder for "From CMS" tab

--- code/HtmlEditorFolder_Toolbar.php
+++ code/HtmlEditorFolder_Toolbar.php
@@ -2,22 +2,45 @@
 
 /**
  * Class HtmlEditorFolder_Toolbar
- * Extension for HtmlEditorField_Toolbar which sets folder name for media form
+ * Extension for HtmlEditorField_Toolbar which sets folder name for media form, including default folder for "From CMS"
  */
 class HtmlEditorFolder_Toolbar extends Extension
 {
 
+	/**
+	 * @return mixed folder path as set by current page, or false if not found
+	 */
+	function getCurrentFolder()
+	{
+		$currentPage = $this->owner->controller->currentPage();
+		return method_exists($currentPage, 'getHtmlEditorFolder') ?
+			$currentPage->getHtmlEditorFolder() :
+			false;
+	}
+
+	/**
+	 * Update the media form, set current folder and use our upload field template (displays upload path)
+	 * @param $form
+	 */
 	function updateMediaForm($form)
 	{
-		if( $uploadField = $form->Fields()->dataFieldByName('AssetUploadField') ){
+		if ($uploadField = $form->Fields()->dataFieldByName('AssetUploadField')) {
+			$uploadField->setFolderName($this->getCurrentFolder());
 			$uploadField->setTemplate('HtmlEditorFolder_UploadField');
-			$currentPage = $this->owner->controller->currentPage();
-			if(
-				method_exists($currentPage, 'getHtmlEditorFolder') &&
-				$folder = $currentPage->getHtmlEditorFolder()
-			){
-				$uploadField->setFolderName($folder);
-			}
+		}
+	}
+
+	/**
+	 * Update parent folder ID for "From CMS" default folder selection
+	 * @param $parentID
+	 */
+	function updateAttachParentID(&$parentID)
+	{
+		if(
+			( $folderPath = $this->getCurrentFolder() ) &&
+			( $parentFolder = Folder::find_or_make($folderPath) )
+		){
+			$parentID = $parentFolder->ID;
 		}
 	}
 
